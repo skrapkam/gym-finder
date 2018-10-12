290e6aaf5be2f0088ed99da0e755128c314b4e15
skrapkam <changsam2017@gmail.com>
2018-10-11 23:44:18 -0700

    new keys

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
-  apiKey: "AIzaSyAMC3HvXCK0vJ-Mh31JQwLjdxzRJAxuIvY",
+  apiKey: "AIzaSyC_u18bSXGueaNI8HyQU9lO_kiHN0CmZH8",
   discoveryDocs:
     ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
   spreadsheetId: "1lmDu2nYFCiVNzclQoY7tStmmCei4i2V8O9grZc9lOc8"
 };
+
